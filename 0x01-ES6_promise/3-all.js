80d251503819ccbd0331e011b2c54dfc43fb07f9
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-09-28 02:22:25 -0700

    Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';;
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]))
-    .then((res) => {console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`)};
+  Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`)})
     .catch((error) => {
       console.log('Signup system offline');
     });
