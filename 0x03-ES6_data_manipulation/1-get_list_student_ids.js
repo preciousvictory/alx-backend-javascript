a81fec40e01e865859757b5b5c2f3357ed479419
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-03 04:12:50 -0700

    corrections

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,5 @@ export default function getListStudentIds(data) {
   if (!Array.isArray(data)) {
     return [];
   }
-  let ids = [];
-  data.map(student => ids.push(student.id));
-  return ids;
+  return data.map(student => student.id);
 }
