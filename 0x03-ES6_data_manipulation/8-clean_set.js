233878a396bd9ac2f889ba315caeb239733831f3
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-03 08:32:33 -0700

    value(), split(), push() to filter

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,2 +1,12 @@
-export default function cleanSet() {
+export default function cleanSet(set, startString) {
+  const like = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (value.startsWith(startString)) {
+      like.push(value.split(startString)[1]);
+    }
+  }
+  return like.join('-');
 }
