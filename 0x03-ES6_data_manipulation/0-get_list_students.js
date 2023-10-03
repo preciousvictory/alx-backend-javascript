fdb66e7cbbb59dc0d3b1232690ae3f1ceaeeda27
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-03 04:26:52 -0700

    corrections

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -5,19 +5,19 @@
 export default function getListStudents() {
   const students = [
     {
-      "id": 1,
-      "firstName": "Guillaume",
-      "location": "San Francisco"
+      id": 1,
+      firstName: "Guillaume",
+      location: "San Francisco"
     },
     {
-      "id": 2,
-      "firstName": "James",
-      "location": "Columbia"
+      id": 2,
+      firstName: "James",
+      location: "Columbia"
     },
     {
-      "id": 5,
-      "firstName": "Serena",
-      "location": "San Francisco"
+      id": 5,
+      firstName: "Serena",
+      location: "San Francisco"
     }
   ];
 
