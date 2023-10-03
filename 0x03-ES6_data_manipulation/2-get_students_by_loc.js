cfd3ccf1ebe91dbc1c459268ee30f1bc8a05d992
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-03 04:58:32 -0700

    corrections

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,6 +2,6 @@ export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) {
     return [];
   }
-  const result = students.filter((student) => student.location === city)
+  const result = students.filter(student => student.location === city)
   return result;
 }
