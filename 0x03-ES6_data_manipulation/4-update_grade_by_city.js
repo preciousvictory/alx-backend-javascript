cd7da42bc74775fdc8d8ef0da408f5e41e415a10
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-03 05:19:02 -0700

    corrections

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const added = students.map((student) => {
-    const grades = newGrades.filter((grades) => student.id === grades.studentId);
-    const value = grades[0] ? grades[0].grade : 'N/A'
+  if (students instanceof Array) {
+    const added = students.map((student) => {
+      const grades = newGrades.filter((grades) => student.id === grades.studentId);
+      const value = grades[0] ? grades[0].grade : 'N/A';
 
-    return {...student, 'grade' : value}
-  })
+      return {...student, 'grade' : value}
+    });
+  }
 
-  const cityOnly = added.filter((student) => student.location === city)
-  return cityOnly;
+  return added.filter((student) => student.location === city)
 }
