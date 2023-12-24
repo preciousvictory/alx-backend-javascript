995fb9437d32a0fd5e188487e427ffc387ad9046
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-12-24 13:51:18 -0800

    child process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-process.stdout.write('Welcome to Holberton School, what is your name?' + '\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
+  const chunk = process.stdin.read();
+
   if (chunk !== null) {
     process.stdout.write('Your name is: ' + chunk);
   }
 });
 
 process.on('exit', function() {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\n');
 });
