c07e96c4e1e4d4a38eb709ac9469650d38660c1a
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-12-26 02:59:49 -0800

    node js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,15 +5,15 @@ const countStudents = (path) => {
     const data = fs.readFileSync(path, 'utf8').trim();
     const rows = data.split("\n");
     rows.shift();
-    let students = []
+    let students = [];
     for (let i = 0; i < rows.length; i++) {
       students.push(rows[i].split(","));
     }
 
     console.log('Number of students: %s', students.length);
-    let fields = []
+    let fields = [];
     for (let i = 0; i < students.length; i++) {
-      const student = students[i]
+      const student = students[i];
       if (!fields.includes(student[student.length - 1])) {
         fields.push(student[student.length - 1]);
       }
@@ -22,15 +22,14 @@ const countStudents = (path) => {
     fields.forEach((field) => {
       person = students.filter((student) => {
         if (student[student.length - 1] == field) {
-		return student
+	  return student;
 	}
       }).map((student) => {
-	return [student[0]].join(",")
-      })
-      console.log('Number of students in %s: %d. List: %s', field, person.length, person.join(', '))
-    })
+	return [student[0]].join(",");
+      });
+      console.log('Number of students in %s: %d. List: %s', field, person.length, person.join(', '));
+    });
   } catch (err) {
-    console.log(err);
     throw new Error('Cannot load the database');
     // console.error(err);
   }
