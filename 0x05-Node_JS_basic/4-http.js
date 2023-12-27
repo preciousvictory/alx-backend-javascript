afb393e2713b290aac62d64ba4abd58dadc29900
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-12-27 02:24:42 -0800

    simple http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,8 +2,15 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.write('Hello Holberton School!');
+  res.end();
 });
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
