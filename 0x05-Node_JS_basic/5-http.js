b32f56c4de5bd2d90c6ce4b9cf6b6b22491e8b20
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-12-27 03:48:47 -0800

    url and endpoints node js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,12 +1,64 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
 const hostname = '127.0.0.1';
 const port = 1245;
+
+const countStudents = (Path) => new Promise((resolve, reject) => {
+  fs.readFile(Path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const rows = data.trim().split('\n');
+      rows.shift();
+      const students = [];
+      rows.forEach((row) => {
+        students.push(row.split(','));
+      });
+
+      console.log('Number of students: %s', students.length);
+
+      const fields = [];
+      students.forEach((student) => {
+        if (!fields.includes(student[student.length - 1])) {
+          fields.push(student[student.length - 1]);
+        }
+      });
+
+      fields.forEach((field) => {
+        let person = students.filter((student) => {
+          if (student[student.length - 1] == field) {
+            return student;
+          }
+        }).map((student) => {
+    	  return [student[0]].join(",");
+        });
+        console.log('Number of students in %s: %d. List: %s', field, person.length, person.join(', '));
+      });
+      resolve(true);
+    }
+  });
+});
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello Holberton School!');
-  res.end();
+  const { url } = req;
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      countStudents(argv[2]);
+      res.end();
+    } catch (err) {
+      res.end(err.message);
+    }
+  }
 });
 
 app.listen(port, hostname, () => {
