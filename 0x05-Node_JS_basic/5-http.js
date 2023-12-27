680edd0a993f1d89e0c207dd5f3bbc84e14c5005
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-12-27 18:47:43 +0000

    nodejs server and readfile to render

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,43 +1,41 @@
 const http = require('http');
 const fs = require('fs');
+const process = require('process');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const countStudents = (Path) => new Promise((resolve, reject) => {
-  fs.readFile(Path, 'utf-8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-    }
-    if (data) {
-      const rows = data.trim().split('\n');
-      rows.shift();
-      const students = [];
-      rows.forEach((row) => {
-        students.push(row.split(','));
-      });
+const countStudents = (Path, response) => {
+  if (!fs.existsSync(Path)) {
+    reject(new Error('Cannot load the database'));
+  } else {
+    const data = fs.readFileSync(Path, 'utf-8');
+    const rows = data.trim().split('\n');
+    rows.shift(1);
+    const students = [];
+    rows.forEach((row) => {
+      students.push(row.split(','));
+    });
+    response.push(`Number of students: ${students.length}`);
 
-      console.log('Number of students: %s', students.length);
+    const fields = [];
+    students.forEach((student) => {
+      if (!fields.includes(student[student.length - 1])) {
+        fields.push(student[student.length - 1]);
+      }
+    });
 
-      const fields = [];
-      students.forEach((student) => {
-        if (!fields.includes(student[student.length - 1])) {
-          fields.push(student[student.length - 1]);
+    fields.forEach((field) => {
+      const person = students.filter((student) => {
+        if (student[student.length - 1] === field) {
+          return student;
         }
-      });
-
-      fields.forEach((field) => {
-        const person = students.filter((student) => {
-          if (student[student.length - 1] === field) {
-            return student;
-          }
-        }).map((student) => return [student[0]].join(","))
-        console.log('Number of students in %s: %d. List: %s', field, person.length, person.join(', '));
-      });
-      resolve(true);
-    }
-  });
-});
+      }).map((student) => {return [student[0]].join(",")});
+      response.push(`Number of students in ${field}: ${person.length}. List: ${person.join(', ')}`);
+    });
+    return response;
+  }
+}
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -47,10 +45,13 @@ const app = http.createServer((req, res) => {
     res.write('Hello Holberton School!');
     res.end();
   }
-  if (url === '/students') {
-    res.write('This is the list of our students\n');
+  else if (url === '/students') {
+    response = ['This is the list of our students'];
     try {
-      countStudents(argv[2]);
+      const report = countStudents(process.argv[2], response);
+      const responseText = report.join('\n');
+      //console.log(report);
+      res.write(Buffer.from(responseText));
       res.end();
     } catch (err) {
       res.end(err.message);
