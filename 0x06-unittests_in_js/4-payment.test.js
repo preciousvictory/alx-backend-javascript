01ab56e74c1b444611beefc4b0cea42023a29ff9
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2024-01-19 23:39:59 +0000

    unittest

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,10 +6,17 @@ const { expect } = require('chai');
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
     const bigBro = sinon.spy(Utils);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
+
+    dummy.returns(10);
 
     sendPaymentRequestToApi(200, 2);
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
     expect(bigBro.calculateNumber.calledWith('SUM', 200, 2)).to.be.true;
     expect(bigBri.calculateNumber.callCount).to.be.equal(1);
+
+    dummy.restore();	  
     bigBro.calculateNumber.restore();
   });
 });
