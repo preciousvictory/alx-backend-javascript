3ed5eefadeb222273ba0f6db48305aed7ec84c6a
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2024-01-20 12:22:08 +0000

    unittest

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,14 +4,14 @@ const sendPaymentRequestToApi = require('./3-payment');
 const { expect } = require('chai');
 
 describe('sendPaymentRequestToApi', () => {
-  const checkSoy = sinon.spy(console, 'log');
+  const check = sinon.spy(console, 'log');
   it('checking if numbers round with spies and stubs', () => {
     sendPaymentRequestToApi(100, 20);
-    const stubBoy = sinon.stub(Utils, 'calculateNumber');
-    stubBoy.withArgs('SUM', 100, 20).returns(120);
-    expect(checkSoy.calledOnce).to.be.true;
+    const stubb = sinon.stub(Utils, 'calculateNumber');
+    stubb.withArgs('SUM', 100, 20).returns(120);
+    expect(check.calledOnce).to.be.true;
     expect(console.log('The total is: 120')).to.be.all;
-    checkSoy.restore();
-    stubBoy.restore();
+    check.restore();
+    stubb.restore();
   });
 });
