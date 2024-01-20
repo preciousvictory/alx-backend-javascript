06fe5f700c0f8814c5c6b4a592a53260cbce4803
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2024-01-20 12:06:19 +0000

    unittest

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,17 +1,18 @@
 const Utils = {
   calculateNumber(type, a, b) {
-    if (type == 'SUM') {
+    if (type === 'SUM') {
         return Math.round(a) + Math.round(b);
-    } else if (type == 'SUBTRACT') {
-        return Math.round(a) - Math.round(b);
-    } else if (type == 'DIVIDE') {
-	if (Math.round(b) == 0) {
-	    return 'Error';
-	}
-        return Math.round(a) / Math.round(b);
-    } else {
-	return 'Error';
     }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    return 0;
   }
 };
 
