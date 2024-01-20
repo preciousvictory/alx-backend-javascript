087ffdcc2f438d14e90eb5135371eabb894dddcb
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2024-01-20 12:12:12 +0000

    unittest

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,18 +4,14 @@ const sendPaymentRequestToApi = require('./3-payment');
 const { expect } = require('chai');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBro = sinon.spy(Utils);
-    const dummy = sinon.stub(Utils, 'calculateNumber');
-
-    dummy.returns(10);
-
-    sendPaymentRequestToApi(200, 2);
-    expect(dummy.calledWith('SUM', 200, 2)).to.be.true;
-    expect(dummy.callCount).to.be.equal(1);
-    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
-    expect(bigBrother.log.callCount).to.be.equal(1);
-    dummy.restore();
-    bigBrother.log.restore();
+  const checkSoy = sinon.spy(console, 'log');
+  it('checking if numbers round with spies and stubs', () => {
+    sendPaymentRequestToApi(100, 20);
+    const stubBoy = sinon.stub(Utils, 'calculateNumber');
+    stubBoy.withArgs('SUM', 100, 20).returns(120);
+    expect(checkSoy.calledOnce).to.be.true;
+    expect(console.log('The total is: 120')).to.be.all;
+    checkSoy.restore();
+    stubBoy.restore();
   });
 });
