21525d10d9ff8f8bcb9888af9d38fa262055f4bf
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2024-01-20 14:25:21 +0000

    node js basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,11 +3,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
-  if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
