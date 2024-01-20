81b9903743b4a383c96b842867bdccfdbf7b30df
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2024-01-20 14:18:50 +0000

    unit test

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
-app.get('/', (_, res) => {
+app.use(express.json());
+
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id(\\d+)', (req, res) => {
   const id = req.params.id;
+
   res.send(`Payment methods for cart ${id}`);
 });
 
